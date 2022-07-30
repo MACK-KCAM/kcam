a049891218ba1eb685ec1b0b3dba08a4c21d6460
Alyssa Chew <alyssachew@MacBook-Pro-2.local>
2022-07-30 15:34:36 -0700

    ability to show an album of two photos of greece currently hardcoded in -- will need to be modified to talk to the database/array of image urls

--- components/TravelPhotos/TravelPhotos.js
+++ components/TravelPhotos/TravelPhotos.js
@@ -7,6 +7,8 @@ import {
     View,
     Plane,
     VrButton,
+    StyleSheet,
+    Image
 } from 'react-360';
 
 export default class TravelPhotos extends React.Component {
@@ -17,10 +19,45 @@ export default class TravelPhotos extends React.Component {
   render () {
     return (
       <View>
-        <Text>Album: </Text>
+        <View style={styles.mainView}>
+          <View style={styles.albumTitleBox}>
+            <Text style={styles.albumTitle}>Album: Greece </Text>
+          </View>
+          <View>
+            <Image style={styles.flag} source={{uri: "static_assets/greece/beach.jpeg"}}></Image>
+            <Image style={styles.flag} source={{uri: "static_assets/greece/santorini.jpeg"}}></Image>
+            {/* <Image src="static_assets/greece/santorini.jpeg"></Image> */}
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+      width: 800,
+      height: 400,
+      backgroundColor: '#eee',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center'
+  },
+  flag: {
+      width: 400,
+      height: '40%'
+  },
+  albumTitleBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  albumTitle: {
+    fontSize: 30,
+  },
+});
 
 AppRegistry.registerComponent('TravelPhotos', () => TravelPhotos);
